162857ac56240f56b64030127b6ad27134f52757
Sudip Das <sudip5428@gmail.com>
2025-06-03 10:34:51 +0530

    code commit for reset and forgot password route add

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(
     function () {
         Route::post('login', [AuthController::class, 'login']);
+        Route::post('forgot-password', [\App\Http\Controllers\Api\Auth\ForgotPasswordController::class, 'sendResetLinkEmail']);
+        Route::post('reset', [\App\Http\Controllers\Api\Auth\ResetPasswordController::class, 'resetPassword']);
 
 
         Route::middleware('auth:api')->group(
